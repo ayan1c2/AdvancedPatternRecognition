ec8ed818a68fb925dd068cc825dd52e2903213c8
ayan1c2 <ayan1.c2@gmail.com>
2020-01-23 13:19:27 +0100

    commit_ayan_23_01_2020

--- Data_generation/fileConversion.py
+++ Data_generation/fileConversion.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-data = pd.read_excel("../data/generated_binary_data.xlsx", index_col=None, header=None)
+data = pd.read_excel("generated_binary_data.xlsx", index_col=None, header=None)
 data = data.transpose()
 
-data.to_csv('../data/glass_artificial.csv', index = False) 
+data.to_csv('glass_artificial.csv', index = False) 
 
